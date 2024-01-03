53fcd0a8f1348e38b7afa62d2a1416c1840f80b6
dhanshreesyn <dhanshreeshingar@gmail.com>
2024-01-03 19:07:24 +0530

    TEST
    TEST

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace Synsofttest\Contactform;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+       $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+}
